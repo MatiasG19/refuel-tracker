7b66ae8fda20082f9eea555346ea340b4dc979f5
MatiasG19 <73800355+MatiasG19@users.noreply.github.com>
2022-11-30 22:23:55 +0100

    Add env vars for flavors

--- quasar.conf.js
+++ quasar.conf.js
@@ -49,6 +49,11 @@ module.exports = configure(function (ctx) {
 
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
+      env: {
+        AD: process.env.AD ? true : false,
+        PAID: process.env.PAID ? true : false,
+      },
+
       vueRouterMode: 'hash', // available values: 'hash', 'history'
 
       // transpile: false,
