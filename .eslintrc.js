757fcee7bcc71bea3357f68a25dec5a5e2b53736
MatiasG19 <m.galusek@gmail.com>
2023-05-06 21:22:37 +0200

    Ignore multi word component names

--- .eslintrc.js
+++ .eslintrc.js
@@ -87,6 +87,11 @@ module.exports = {
     '@typescript-eslint/unbound-method': 'off',
 
     // allow debugger during development only
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+
+    // Vue
+    'vue/multi-word-component-names': ['error', {
+      'ignores': []
+    }]
   }
 }
