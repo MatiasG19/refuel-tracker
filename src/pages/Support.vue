375942536d1194263df11430024f178bd3e0ee9b
MatiasG19 <m.galusek@gmail.com>
2023-08-27 10:38:51 +0200

    Update support page

--- src/pages/Support.vue
+++ src/pages/Support.vue
@@ -7,26 +7,7 @@
       <q-icon name="rocket_launch" color="accent"></q-icon>
     </h5>
 
-    <q-card class="space-station">
-      <q-card-section>
-        <div class="q-mt-md text-center">
-          <div class="q-mb-md text-center">
-            {{ packageJson.productName }}'s code is open source! Check it out!
-          </div>
-          <q-btn
-            color="accent"
-            icon-right="code"
-            label="GitHub"
-            @click="openURL(repositoryLink)"
-            no-caps
-            outline
-          />
-        </div>
-      </q-card-section>
-    </q-card>
-
     <q-carousel
-      v-if="false"
       v-model="slide"
       transition-prev="jump-right"
       transition-next="jump-left"
@@ -78,13 +59,54 @@
           />
         </div>
       </q-carousel-slide>
+      <q-carousel-slide name="issue" class="column no-wrap flex-center">
+        <div class="q-mt-md text-center">
+          <div class="column items-center">
+            <div class="q-mb-md text-center">
+              Having issues or improvement ideas?
+            </div>
+            <q-btn
+              color="accent"
+              icon-right="bug_report"
+              label="GitHub"
+              @click="openURL(issueLink)"
+              no-caps
+              outline
+            />
+          </div>
+        </div>
+      </q-carousel-slide>
+      <q-carousel-slide name="star" class="column no-wrap flex-center">
+        <div class="q-mt-md text-center">
+          <div class="column items-center">
+            <div class="q-mb-md text-center">
+              Like the project? Leave a star!
+            </div>
+            <q-btn
+              color="accent"
+              icon-right="star"
+              label="GitHub"
+              @click="openURL(repositoryLink)"
+              no-caps
+              outline
+            />
+          </div>
+        </div>
+      </q-carousel-slide>
     </q-carousel>
 
     <div class="q-mt-md full-width text-center">
       {{ packageJson.productName }} version {{ packageJson.version }}
     </div>
     <div class="q-mt-md full-width text-center">
-      View license on <a :href="licenseLink">Github</a>
+      <q-btn
+        size="sm"
+        color="accent"
+        label="View license on GitHub"
+        @click="openURL(licenseLink)"
+        no-caps
+        outline
+      />
     </div>
   </q-page>
 </template>
@@ -99,10 +121,11 @@ import { emitter } from 'src/boot/mitt'
 const router = useRouter()
 const routePath = computed(() => router.currentRoute.value.path)
 const ratingModel = ref(3)
-const slide = ref('feedback')
+const slide = ref('code')
 const repositoryLink = 'https://github.com/MatiasG19/refuel-tracker'
 const licenseLink =
   'https://github.com/MatiasG19/refuel-tracker/blob/main/LICENSE'
+const issueLink = 'https://github.com/MatiasG19/refuel-tracker/issues/new'
 
 const emits = defineEmits(['update:title'])
 
