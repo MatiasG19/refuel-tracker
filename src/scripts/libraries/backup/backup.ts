e7bebb9246560c5ab50bb2200055a39361de4bc3
MatiasG19 <m.galusek@gmail.com>
2023-08-27 22:37:40 +0200

    Disable mobile platfrom specific features on web

--- src/scripts/libraries/backup/backup.ts
+++ src/scripts/libraries/backup/backup.ts
@@ -3,6 +3,7 @@ import { db } from 'src/boot/dexie'
 import { Filesystem, Encoding } from '@capacitor/filesystem'
 import { getDateForFileName } from '../utils'
 import { FilePicker } from 'src/plugins/capacitor-file-picker'
+import { Platform } from 'quasar'
 
 export async function exportDB(path: string) {
   const blob = await db.export()
@@ -16,26 +17,30 @@ export async function importDB(path: string) {
 
 async function writeBlob(blob: Blob, path: string) {
   const dataAsString = await blob.text()
-  FilePicker.createFile({
-    path: path,
-    fileName: `RefuelTrackerBackup_${getDateForFileName()}.json`,
-    mimeType: '*/*',
-    data: dataAsString
-    // TODO encoding
-    // encoding: Encoding.UTF8
-  })
+  if (Platform.is.mobile) {
+    FilePicker.createFile({
+      path: path,
+      fileName: `RefuelTrackerBackup_${getDateForFileName()}.json`,
+      mimeType: '*/*',
+      data: dataAsString
+      // TODO encoding
+      // encoding: Encoding.UTF8
+    })
+  }
 }
 
 async function readBlob(path: string): Promise<Blob> {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-  const result = await Filesystem.readFile({
-    path: path,
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
-    encoding: Encoding.UTF8
-  })
-
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-  return new Blob([result.data], {
-    type: 'application/json'
-  })
+  if (Platform.is.mobile) {
+    const result = await Filesystem.readFile({
+      path: path,
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+      encoding: Encoding.UTF8
+    })
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    return new Blob([result.data], {
+      type: 'application/json'
+    })
+  }
+  return Promise.resolve(new Blob())
 }
