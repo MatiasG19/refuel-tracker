8d9df39d84a82769cdec0fe4f611be0aec497aa9
MatiasG19 <73800355+MatiasG19@users.noreply.github.com>
2023-09-01 21:21:06 +0200

    feat: Add demo data (#87)

--- src/boot/dexie.ts
+++ src/boot/dexie.ts
@@ -20,9 +20,12 @@ export class RefuelTrackerDexie extends Dexie {
         '++id, colorThemeId, distanceUnitId, vehicleId, plateNumberInTitleActive, autoBackupActive, autoBackupPath, refuelFilterActive'
     })
 
-    // Insert static data
-    this.insertGraphSettings()
-    this.insertSettings()
+    // Only called on very first database creation
+    this.on('populate', () => {
+      this.insertGraphSettings()
+      this.insertDemoData()
+      this.insertSettings()
+    })
   }
 
   insertGraphSettings() {
@@ -37,30 +40,57 @@ export class RefuelTrackerDexie extends Dexie {
 
     settings.forEach(s => {
       ;(async () => {
-        const settings = await this.graphSettings
-          .where('uid')
-          .equals(s.uid)
-          .toArray()
-        if (settings.length === 0) await this.graphSettings.put(s)
+        await this.graphSettings.put(s)
       })()
     })
   }
 
-  insertSettings() {
+  insertDemoData() {
+    const vehicle = new Vehicle()
+    vehicle.name = 'My Vehicle'
+    vehicle.plateNumber = 'MYNUMBERPLATE'
+    vehicle.currencyUnit = '€'
+    vehicle.fuelUnitId = 1
     ;(async () => {
-      if ((await this.settings.count()) === 0) {
-        const settings = new Settings()
-        settings.colorThemeId = 1
-        settings.distanceUnitId = 1
-        settings.plateNumberInTitleActive = false
-        settings.autoBackupActive = false
-        settings.autoBackupPath = ''
-        settings.refuelFilterActive = false
-        {
-          ;(async () => await this.settings.put(settings))()
-        }
-      }
+      vehicle.id = (await this.vehicles.put(vehicle)) as number
+      const refuels = []
+      let refuel = new Refuel()
+      refuel.date = new Date()
+      refuel.refueledAmount = 33
+      refuel.payedAmount = 55
+      refuel.distanceDriven = 650
+      refuel.vehicleId = vehicle.id
+      refuels.push(refuel)
+
+      refuel = new Refuel()
+      refuel.date = new Date()
+      refuel.refueledAmount = 44
+      refuel.payedAmount = 80
+      refuel.distanceDriven = 900
+      refuel.vehicleId = vehicle.id
+      refuels.push(refuel)
+
+      refuel = new Refuel()
+      refuel.date = new Date()
+      refuel.refueledAmount = 50
+      refuel.payedAmount = 90
+      refuel.distanceDriven = 850
+      refuel.vehicleId = vehicle.id
+      refuels.push(refuel)
+      await this.refuels.bulkPut(refuels)
     })()
+  }
+
+  insertSettings() {
+    const settings = new Settings()
+    settings.vehicleId = 1
+    settings.colorThemeId = 1
+    settings.distanceUnitId = 1
+    settings.plateNumberInTitleActive = false
+    settings.autoBackupActive = false
+    settings.autoBackupPath = ''
+    settings.refuelFilterActive = false
+    ;(async () => await this.settings.put(settings))()
   }
 }
 
