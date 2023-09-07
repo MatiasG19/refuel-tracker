d855a0ef6323c55ddda3c2dee524223fbacbc520
MatiasG19 <m.galusek@gmail.com>
2023-09-07 13:54:39 +0200

    Create script

---
+++ modifyPackageJson.mjs
@@ -0,0 +1,25 @@
+import fs from 'fs'
+import path from 'path'
+import process from 'process'
+
+function modifyPackageJson(
+  version
+) {
+  const filePath = path.resolve(process.cwd(), 'package.json')
+  const file = fs.readFileSync(filePath, 'utf8')
+  const newFile = JSON.parse(file)
+  newFile['version'] = version
+
+  fs.writeFile(filePath, JSON.stringify(newFile, null, 2), err => {
+    if (err) {
+      throw new Error(err.message)
+    }
+  })
+}
+
+function main() {
+  var args = process.argv.slice(2);
+  modifyPackageJson(args[0])
+}
+
+main()
