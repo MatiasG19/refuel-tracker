bdc44f41e5c33863f29b306f3c91a411a29fa72c
MatiasG19 <m.galusek@gmail.com>
2023-09-10 15:14:57 +0200

    Remove unused function

--- src/boot/storageManager.ts
+++ src/boot/storageManager.ts
@@ -19,56 +19,6 @@ async function persist() {
     : undefined
 }
 
-/** Queries available disk quota.
-    @see https://developer.mozilla.org/en-US/docs/Web/API/StorageEstimate
-    @returns {Promise<{quota: number, usage: number}>} Promise resolved with
-    {quota: number, usage: number} or undefined.
-  */
-async function showEstimatedQuota() {
-  // ts-ignore @typescript-eslint/await-thenable
-  return navigator.storage && navigator.storage.estimate
-    ? await navigator.storage.estimate()
-    : undefined
-}
-
-/** Tries to persist storage without ever prompting user.
-    @returns {Promise<string>}
-      "never" In case persisting is not ever possible. Caller don't bother
-        asking user for permission.
-      "prompt" In case persisting would be possible if prompting user first.
-      "persisted" In case this call successfully silently persisted the storage,
-        or if it was already persisted.
-  */
-async function tryPersistWithoutPromtingUser() {
-  // if (!navigator.storage || !navigator.storage.persisted) {
-  //   return 'never'
-  // }
-  // let persisted = await navigator.storage.persisted()
-  // if (persisted) {
-  //   return 'persisted'
-  // }
-  // if (!navigator.permissions || !navigator.permissions.query) {
-  //   return 'prompt' // It MAY be successful to prompt. Don't know.
-  // }
-  const permission = await navigator.permissions.query({
-    name: 'persistent-storage'
-  })
-  if (permission.state === 'granted') {
-    const persisted = await navigator.storage.persist()
-    console.log('granted')
-
-    if (persisted) {
-      return 'persisted'
-    } else {
-      throw new Error('Failed to persist')
-    }
-  }
-  if (permission.state === 'prompt') {
-    return 'prompt'
-  }
-  return 'never'
-}
-
 export async function initStoragePersistence(): Promise<boolean> {
   const permission = await navigator.permissions.query({
     name: 'persistent-storage'
