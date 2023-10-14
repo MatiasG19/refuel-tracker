0213255a41074c01b03051575434a3561175b9d4
MatiasG19 <m.galusek@gmail.com>
2023-10-14 14:21:00 +0200

    Initialize settings in boot

--- src/boot/init.ts
+++ src/boot/init.ts
@@ -1,10 +1,11 @@
-import { useRefuelFilterStore } from 'src/pages/refuels/stores/refuelFilterStore'
+import { initSettings } from 'src/scripts/initSettings'
+initSettings()
 
+import { useRefuelFilterStore } from 'src/pages/refuels/stores/refuelFilterStore'
 const refuelFilterStore = useRefuelFilterStore()
 refuelFilterStore.readFilter()
 
 import { registerGraphData } from 'src/pages/graphData/scripts/registerGraphData'
 import { registerFuelConsumption } from 'src/scripts/libraries/refuel/functions/fuelConsumption/registerFuelConsumption'
-
 registerGraphData()
 registerFuelConsumption()
