2afdae89ab29d4e428770eef3da1bdda1994314f
MatiasG19 <m.galusek@gmail.com>
2024-03-08 20:12:09 +0100

    Translate support page

--- src/i18n/de/index.ts
+++ src/i18n/de/index.ts
@@ -31,14 +31,37 @@ export default {
   pages: {
     vehicles: {
       vehicles: {
-        addVehicle: 'Fahrzeug hinzufügen'
+        title: 'Fahrzeuge',
+        addVehicle: 'Fahrzeug hinzufügen',
+        optionsDialog: {
+          showRefuels: 'Zeige Betankungen',
+          edit: 'Editieren',
+          delete: 'Löschen',
+          deleteVehicle: 'Fahrzeug löschen?',
+          deletingVehicle: 'Fahrzeug wird gelöscht'
+        }
       },
       vehicleForm: {
         vehicleName: 'Fahrzeugname',
         licensePlate: 'Kennzeichen',
         fuelUnit: 'Kraftstoffeinheit',
-        currencyUnit: 'Währung'
+        currencyUnit: 'Währung',
+        titleAddVehicle: 'Fahrzeug hinzufügen',
+        titleEditVehicle: 'Fahrzeug editieren'
       }
+    },
+    support: {
+      title: 'Unterstützung',
+      greetingPart1: 'Danke',
+      greetingPart2: 'fürs Benutzen des',
+      greetingPart3: '! Ich hoffe Dir gefällt die App soweit',
+      slide1: 's Code ist open source! Schaue mal rein!',
+      slide2: 'Du hast Problem oder Verbesserungsideen?',
+      slide3: 'Gefällt Dir das  Projekt? Schenke einen Stern!',
+      slide4:
+        'Bewerte die App oder hinterlasse Feedback, um die App zu verbessern!',
+      version: 'version',
+      btnLicense: 'View license on GitHub'
     }
   }
 }
--- src/i18n/en/index.ts
+++ src/i18n/en/index.ts
@@ -49,6 +49,18 @@ export default {
         titleAddVehicle: 'Add vehicle',
         titleEditVehicle: 'Edit vehicle'
       }
+    },
+    support: {
+      greetingPart1: 'Thank you',
+      greetingPart2: 'for using',
+      greetingPart3: '! I hope you have been enjoying the app so far',
+      slide1: "'s code is open source! Check it out!",
+      slide2: 'Having issues or improvement ideas?',
+      slide3: 'Like the project? Leave a star!',
+      slide4:
+        'Rate this app if you like it or leave feedback to improve your experience!',
+      version: 'version',
+      btnLicense: 'View license on GitHub'
     }
   }
 }
