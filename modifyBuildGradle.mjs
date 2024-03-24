068b9b300515e088ae4daee076852fb2fd5d5266
MatiasG19 <m.galusek@gmail.com>
2024-03-24 15:20:49 +0100

    Add log

--- modifyBuildGradle.mjs
+++ modifyBuildGradle.mjs
@@ -13,6 +13,7 @@ function modifyPackageJson(version) {
   let file = fs.readFileSync(filePath, 'utf8')
   const versionArr = version.split('.')
   const versionCode = parseInt(versionArr[0]) * 1000000 + parseInt(versionArr[1]) * 1000 + parseInt(versionArr[2])
+  console.log(`Generated versionCode: ${versionCode}`)
   file = file
     .replace(/versionCode 1/g, `versionCode ${versionCode}`)
     .replace(/versionName "1.0"/g, `versionName "${version}"`)
