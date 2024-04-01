43ff4129405dc831aa464b46883d606e0f0d6f63
MatiasG19 <m.galusek@gmail.com>
2024-04-01 12:24:23 +0200

    Update de.ts

--- src/pages/support/i18n/de.ts
+++ src/pages/support/i18n/de.ts
@@ -5,7 +5,7 @@ export default {
   greetingPart3: '! Ich hoffe Dir gefällt die App soweit',
   slide1: 's Code ist open source! Schaue mal rein!',
   slide2: 'Du hast Problem oder Verbesserungsideen?',
-  slide3: 'Gefällt Dir das  Projekt? Schenke einen Stern!',
+  slide3: 'Gefällt Dir das Projekt? Schenke einen Stern!',
   slide4:
     'Bewerte die App oder hinterlasse Feedback, um die App zu verbessern!',
   version: 'Version',
