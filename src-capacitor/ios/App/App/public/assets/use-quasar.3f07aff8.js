5004a9f440fb1ec5912fc47e6ac5840ae72f7076
MatiasG19 <m.galusek@gmail.com>
2024-04-01 14:55:03 +0200

    Sync capacitor

---
+++ src-capacitor/ios/App/App/public/assets/use-quasar.3f07aff8.js
@@ -0,0 +1,1 @@
+import{i as a,aF as r}from"./index.af9e0f6e.js";function u(){return a(r)}export{u};
---
+++ src-capacitor/ios/App/App/public/assets/web.bd759c4a.js
@@ -0,0 +1,1 @@
+import{W as n}from"./index.af9e0f6e.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
