e001225d39c16ccb1c7d9619bbdf635ef1e6e03f
MatiasG19 <73800355+MatiasG19@users.noreply.github.com>
2024-04-13 20:35:17 +0200

    remove(notification): Update notification (#292)

--- src-capacitor/ios/App/App/public/assets/VehicleLayout.d0a18f0e.js
+++
@@ -1,1 +0,0 @@
-import{$ as o,aa as r,a1 as a,a2 as t}from"./index.af9e0f6e.js";const c={};function n(s,_){const e=r("router-view");return a(),t(e)}var i=o(c,[["render",n]]);export{i as default};
--- src/boot/checkForUpdate.ts
+++
@@ -1,5 +0,0 @@
-import { useCheckForUpdate } from 'src/scripts/libraries/utils'
-
-const checkForUpdate = useCheckForUpdate()
-
-checkForUpdate()
