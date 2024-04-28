f5111a7ebc7e92eab513aaee05731fca152bcb01
MatiasG19 <m.galusek@gmail.com>
2024-04-28 12:25:57 +0200

    Revert "Draft chart"
    This reverts commit 9d516979cfd08671b309228db5511cdc4dda3a33.

--- src/pages/graphData/GraphPage.vue
+++ src/pages/graphData/GraphPage.vue
@@ -1,36 +1,19 @@
 <template>
-  <div>
-    TODO
-    <div>- Filter Selector</div>
-    <div>- data selector (distance driven, fuel burnt, etc.)</div>
-    <div>- compare data (e.g. compare this month with last)</div>
-    <div>- Vertical view</div>
-  </div>
   <q-page>
-    <Bar id="my-chart-id" :options="chartOptions" :data="chartData" />
+    Test
+    <!-- <Bar id="my-chart-id" :options="chartOptions" :data="chartData" /> -->
   </q-page>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-import { Bar } from 'vue-chartjs'
-import {
-  Chart as ChartJS,
-  Title,
-  Tooltip,
-  Legend,
-  BarElement,
-  CategoryScale,
-  LinearScale
-} from 'chart.js'
+// import { ref } from 'vue'
+// import { Bar } from 'vue-chartjs'
 
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
-
-const chartData = ref({
-  labels: ['January', 'February', 'March'],
-  datasets: [{ data: [40, 20, 12] }]
-})
-const chartOptions = ref({
-  responsive: true
-})
+// const chartData = ref({
+//   labels: ['January', 'February', 'March'],
+//   datasets: [{ data: [40, 20, 12] }]
+// })
+// const chartOptions = ref({
+//   responsive: true
+// })
 </script>
