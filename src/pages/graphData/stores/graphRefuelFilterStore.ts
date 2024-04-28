3938f7026b426a7d6f5344d3ef57f6085884fe95
MatiasG19 <m.galusek@gmail.com>
2024-04-28 10:08:09 +0200

    Create graphRefuelFilterStore.ts

---
+++ src/pages/graphData/stores/graphRefuelFilterStore.ts
@@ -0,0 +1,60 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+import { date } from 'quasar'
+import { refuelFilterRepository } from 'src/scripts/databaseRepositories'
+
+export const useGraphRefuelFilterStore = defineStore(
+  'graphRefuelFilterStore',
+  () => {
+    const filterName = ref<string>('')
+    const filterActive = ref<boolean>(false)
+    const dateFrom = ref<Date>(new Date())
+    const dateUntil = ref<Date>(new Date())
+    const filterId = 1
+
+    function setFilter() {
+      ;(async () => {
+        const filter = await refuelFilterRepository.readFilterById(filterId)
+        if (!filter) return
+        filter.name = filterName.value =
+          date.formatDate(dateFrom.value, 'YYYY/MM/DD') +
+          ' - ' +
+          date.formatDate(dateUntil.value, 'YYYY/MM/DD')
+        filter.active = filterActive.value = true
+        filter.dateFrom = dateFrom.value
+        filter.dateUntil = dateUntil.value
+        await refuelFilterRepository.setFilter(filter)
+      })()
+    }
+
+    function removeFilter() {
+      ;(async () => {
+        const filter = await refuelFilterRepository.readFilterById(filterId)
+        if (!filter) return
+        filter.active = filterActive.value = false
+        await refuelFilterRepository.setFilter(filter)
+      })()
+    }
+
+    function readFilter() {
+      ;(async () => {
+        const filter = await refuelFilterRepository.readFilterById(filterId)
+        if (!filter) return
+        filterName.value = filter.name
+        filterActive.value = filter.active
+        dateFrom.value = filter.dateFrom
+        dateUntil.value = filter.dateUntil
+      })()
+    }
+
+    return {
+      filterName,
+      filterActive,
+      dateFrom,
+      dateUntil,
+      setFilter,
+      removeFilter,
+      readFilter
+    }
+  }
+)
