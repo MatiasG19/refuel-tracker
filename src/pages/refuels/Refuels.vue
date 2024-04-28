54bee703ecdb6d9fe923f1443c60103f797320f2
MatiasG19 <m.galusek@gmail.com>
2024-04-28 12:22:23 +0200

    Use refuel repo, simplify store

--- src/pages/refuels/Refuels.vue
+++ src/pages/refuels/Refuels.vue
@@ -69,8 +69,10 @@
         <refuel-card
           :key="index"
           :refuel="item"
-          :vehicle="vehicle"
-          :fuelConsumption="vehicleFuelConsumption(vehicle, item).toFixed(2)"
+          :vehicle="refuelStore.vehicle"
+          :fuelConsumption="
+            vehicleFuelConsumption(refuelStore.vehicle, item).toFixed(2)
+          "
           :loading="loading"
           class="q-pt-md q-pl-md q-pr-md"
         />
@@ -87,22 +89,20 @@ import { emitter } from 'src/boot/mitt'
 import { optionsDialog } from 'src/components/dialogs/optionsDialog'
 import { confirmDialog } from 'src/components/dialogs/confirmDialog'
 import { useSettingsStore } from 'src/pages/settings/stores/settingsStore'
-import { useRefuelStore } from 'src/stores/refuelStore'
-import { Refuel, Vehicle } from 'src/scripts/libraries/refuel/models'
+import { Refuel } from 'src/scripts/libraries/refuel/models'
 import { vehicleFuelConsumption } from 'src/scripts/libraries/refuel/functions/vehicle'
 import { QVirtualScroll } from 'quasar'
-import { useRefuelFilterStore } from './stores/refuelFilterStore'
+import { useRefuelStore, useRefuelFilterStore } from './stores'
 import { useI18n } from 'vue-i18n'
 import { i18n } from 'src/boot/i18n'
 import messages from './i18n'
 
 const router = useRouter()
-const refuelStore = useRefuelStore()
 const settingsStore = useSettingsStore()
+const refuelStore = useRefuelStore()
 const refuelFilterStore = useRefuelFilterStore()
 const { t } = useI18n({ useScope: 'local', messages })
 
-const vehicle = ref<Vehicle>(new Vehicle())
 const vehiclesExists = settingsStore.selectedVehicleId
 const vehicleName = ref<string>('')
 const loading = ref(true)
@@ -164,12 +164,7 @@ emitter.on('showRefuelOptionsDialog', id =>
             ;(async () =>
               refuelStore
                 .deleteRefuel(id)
-                .then(
-                  async () =>
-                    await refuelStore.readRefuels(
-                      settingsStore.selectedVehicleId ?? 0
-                    )
-                ))()
+                .then(async () => await refuelStore.readData()))()
           },
           id
         )
@@ -184,10 +179,7 @@ onBeforeMount(async () => {
     ? settingsStore.selectedVehiclePlateNumber
     : settingsStore.selectedVehicleName
 
-  await refuelStore.readRefuels(settingsStore.selectedVehicleId ?? 0)
-  vehicle.value =
-    (await refuelStore.getVehicle(settingsStore.selectedVehicleId ?? 0)) ??
-    vehicle.value
+  await refuelStore.readData()
 
   // Define to which index to scroll
   if (props.id) {
--- src/pages/refuels/stores/refuelStore.ts
+++ src/pages/refuels/stores/refuelStore.ts
@@ -1,48 +1,43 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
-import { db } from 'src/boot/dexie'
 import { Refuel, Vehicle } from 'src/scripts/libraries/refuel/models'
-import { useSettingsStore } from 'src/pages/settings/stores/settingsStore'
-import { useVehicleStore } from 'src/pages/vehicles/stores/vehicleStore'
+import { useSettingsStore } from 'src/pages/settings/stores'
+import { useVehicleStore } from 'src/pages/vehicles/stores'
+import { refuelRepository } from 'src/scripts/databaseRepositories'
 
 export const useRefuelStore = defineStore('refuelStore', () => {
-  const settings = useSettingsStore()
+  const settingsStore = useSettingsStore()
   const vehicleStore = useVehicleStore()
   const refuels = ref<Refuel[]>([])
-  const vehicle = ref<Vehicle | null>(null)
+  const vehicle = ref<Vehicle>(new Vehicle())
 
-  async function read() {
-    if (!settings.selectedVehicleId) return
-    refuels.value = await getRefuels(settings.selectedVehicleId)
-    vehicle.value = await vehicleStore.getVehicle(settings.selectedVehicleId)
-  }
-
-  async function getRefuels(vehicleId: number): Promise<Refuel[]> {
-    return await db.refuels.where('vehicleId').equals(vehicleId).toArray()
-  }
-
-  async function getRefuel(id: number): Promise<Refuel | null> {
-    if (!settings.selectedVehicleId) return null
-    const refuels = await getRefuels(settings.selectedVehicleId)
-    return refuels.find(v => v.id == id) ?? null
+  async function readData() {
+    if (!settingsStore.selectedVehicleId) return
+    vehicle.value =
+      (await vehicleStore.getVehicle(settingsStore.selectedVehicleId)) ??
+      vehicle.value
+    refuels.value = await refuelRepository.getRefuelsForVehicle(
+      vehicle.value.id
+    )
+    await Promise.resolve()
   }
 
   async function addRefuel(refuel: Refuel): Promise<number> {
-    return (await db.refuels.add(refuel)) as number
+    return await refuelRepository.addRefuel(refuel)
   }
 
   async function updateRefuel(refuel: Refuel) {
-    await db.refuels.put(refuel)
+    await refuelRepository.updateRefuel(refuel)
   }
 
   async function deleteRefuel(id: number) {
-    await db.refuels.delete(id)
+    await refuelRepository.deleteRefuel(id)
   }
 
   return {
     refuels,
-    read,
-    getRefuel,
+    vehicle,
+    readData,
     addRefuel,
     updateRefuel,
     deleteRefuel
