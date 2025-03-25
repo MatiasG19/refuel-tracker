b782ba9bec35d1b2437358961682e31852fc91dd
MatiasG19 <m.galusek@gmail.com>
2025-03-25 23:26:33 +0100

    fix colors and color switching

--- src-capacitor/android/app/src/main/java/capacitor/quasar/refueltracker/plugins/themesetter/ThemeSetter.java
+++ src-capacitor/android/app/src/main/java/capacitor/quasar/refueltracker/plugins/themesetter/ThemeSetter.java
@@ -1,8 +1,11 @@
 package capacitor.quasar.refueltracker.plugins.themesetter;
 
-import static androidx.core.app.ActivityCompat.recreate;
+import android.os.Build;
+import android.view.Window;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.core.content.ContextCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
+import androidx.core.view.WindowCompat;
 import capacitor.quasar.refueltracker.R;
 
 public class ThemeSetter {
@@ -16,15 +19,38 @@ public class ThemeSetter {
     switch (themeId) {
       case 0:
         changeTheme(R.style.AppTheme);
+        break;
       case 1:
         changeTheme(R.style.Theme_Light);
+        break;
       case 2:
         changeTheme(R.style.Theme_Dark);
+        break;
     }
   }
 
   public void changeTheme(int themeId) {
-    setTheme(themeId);
-    recreate(this.app);
-}
+    app.runOnUiThread(() -> {
+      Window window = app.getWindow();
+      WindowInsetsControllerCompat windowInsetsControllerCompat = WindowCompat.getInsetsController(window, window.getDecorView());
+      if(themeId == R.style.Theme_Light) {
+        windowInsetsControllerCompat.setAppearanceLightNavigationBars(true);
+        windowInsetsControllerCompat.setAppearanceLightStatusBars(true);
+        if(Build.VERSION.SDK_INT <= Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
+          window.setNavigationBarColor(ContextCompat.getColor(app.getApplicationContext(), R.color.colorPrimary_light));
+      }
+      else if(themeId == R.style.Theme_Dark) {
+        windowInsetsControllerCompat.setAppearanceLightNavigationBars(false);
+        windowInsetsControllerCompat.setAppearanceLightStatusBars(false);
+        if(Build.VERSION.SDK_INT <= Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
+          window.setNavigationBarColor(ContextCompat.getColor(app.getApplicationContext(), R.color.colorPrimary_dark));
+      }
+      else {
+        windowInsetsControllerCompat.setAppearanceLightNavigationBars(false);
+        windowInsetsControllerCompat.setAppearanceLightStatusBars(false);
+        if(Build.VERSION.SDK_INT <= Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
+          window.setNavigationBarColor(ContextCompat.getColor(app.getApplicationContext(), R.color.colorPrimary));
+      }
+    });
+  }
 }
