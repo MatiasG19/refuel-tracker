073b8f38f8f7ebe952ec037244c7a7e75238e11a
MatiasG19 <m.galusek@gmail.com>
2025-04-19 14:25:36 +0200

    fix calculating chart total cost

--- src/pages/dashboard/components/chart/scripts/models/chartDataFunctions/TotalMoneySpent.ts
+++ src/pages/dashboard/components/chart/scripts/models/chartDataFunctions/TotalMoneySpent.ts
@@ -1,4 +1,8 @@
-import type { Vehicle, Refuel } from 'src/scripts/libraries/refuel/models'
+import type {
+  Vehicle,
+  Refuel,
+  Expense
+} from 'src/scripts/libraries/refuel/models'
 import { AbstractChartData, type Group, type IChartData } from '../index.js'
 import { groupBy as groupByFun } from '../groupBy.js'
 import { vehicleTotalMoneySpent } from 'src/scripts/libraries/refuel/functions/vehicle'
@@ -7,14 +11,31 @@ export class TotalMoneySpent extends AbstractChartData {
   override getChartData(
     groupBy: Group,
     vehicle: Vehicle,
-    refuels: Refuel[]
+    refuels: Refuel[],
+    expenses: Expense[]
   ): IChartData {
-    const groupedData = groupByFun(refuels, groupBy)
+    const groupedData = groupByFun(
+      [
+        // Convert refuels to expenses
+        ...refuels.map(r => {
+          return {
+            id: r.id,
+            date: r.date,
+            payedAmount: r.payedAmount,
+            description: '',
+            vehicleId: r.vehicleId
+          } as Expense
+        }),
+        ...expenses
+      ],
+      groupBy
+    )
     const chartData: IChartData = { labels: [], data: [] }
     for (const key in groupedData) {
       chartData.labels.push(key)
       const v = { ...vehicle }
-      v.refuels = groupedData[key]! as Refuel[]
+      v.refuels = []
+      v.expenses = groupedData[key]! as Expense[]
       chartData.data.push(vehicleTotalMoneySpent(v).toFixedIfNotZero(2))
     }
 
